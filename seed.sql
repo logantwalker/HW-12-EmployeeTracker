aa5daafefe51a424abf9da8116c1824be68955e3
L.Walker <logantwalker95@gmail.com>
2020-08-27 20:47:26 -0400

    seed.sql

--- seed.sql
+++ seed.sql
@@ -6,15 +6,36 @@ USE hw11_DB;
 
 CREATE TABLE departments (
   id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
   id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  deparment_id INT NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE employees (
   id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NOT NULL,
   PRIMARY KEY (id)
 );
+
+INSERT INTO departments
+VALUE (default,'Executive');
+
+INSERT INTO departments
+VALUE (default, 'Engineering');
+
+INSERT INTO departments
+VALUE (default, 'Human Resources');
+
+INSERT INTO departments
+VALUE (default, '');
+
