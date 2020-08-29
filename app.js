a14bd7244bd0dc822d9abe82491d77a158046caa
L.Walker <logantwalker95@gmail.com>
2020-08-29 12:46:01 -0400

    testing with inquirer

--- app.js
+++ app.js
@@ -15,6 +15,35 @@ connection.connect(function(err) {
 });
 
 function startPromt(){
+  inquirer
+    .prompt([{
+        type: 'list',
+        message: '\nWhat would you like to do?',
+        choices:['View All Employees','View All Departments','View All Roles','Quit'],
+        name: 'action'
+    }])
+    .then(function(res){
+
+      interpret(res.action)
+      
+    }).catch((error)=>{console.log(error)});
+};
+
+const interpret = (res) =>{
+  switch(res){
+    case 'View All Employees':
+      employees.getEmployee(connection);
+      break;
+    case 'View All Roles':
+      roles.getRoles(connection);
+      break;
+    case 'View All Departments':
+      department.getDepartments(connection);
+      break;
+    case 'Quit':
+      connection.end()
+      return
+  }
+  startPromt()
 
 }
-
